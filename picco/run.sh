04aaaa84cc6223c0506a021c85a6f58bed8da6a4
Marcel Keller <mks.keller@gmail.com>
2020-05-03 14:47:54 +1000

    PICCO benchmark.

---
+++ picco/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd tests
+
+python ~/source/geninput.py -e innerprod -l 100000
+
+cd innerprod
+
+cat data/input.[AB].dat > data/input
+
+picco-utility -I 1 data/input util_config secure_server/share
+
+cd secure_server
+
+time ./innerprod 3 run_config keys/private3.pem 1 1 share3 output & true
+sleep 1
+time ./innerprod 2 run_config keys/private2.pem 1 1 share2 output & true
+sleep 1
+time ./innerprod 1 run_config keys/private1.pem 1 1 share1 output & true
+sleep 1
+
+picco-seed run_config ../util_config
+
+wait
+
+picco-utility -O 1 output ../util_config result
+
+cat result
--- picco/source/innerprod/innerprod.c
+++ picco/source/innerprod/innerprod.c
@@ -2,14 +2,14 @@
  * takes the inner product of two inputs
  */
 
-public int LEN = 10;
+public int LEN = 100000;
 
 public int main() {
 
     int A[LEN], B[LEN];
 
     smcinput(A,1,LEN);
-    smcinput(B,2,LEN);
+    smcinput(B,1,LEN);
 
     int p = A @ B;
 
