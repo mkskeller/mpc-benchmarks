fd7bfee8750ddbde456489e267eb6ae5560dfa52
Marcel Keller <mks.keller@gmail.com>
2020-08-14 17:32:49 +1000

    Use latest version because of dependencies.

--- aby3/install.sh
+++ aby3/install.sh
@@ -2,7 +2,7 @@
 cd ~
 git clone https://github.com/ladnir/aby3.git
 cd aby3
-git checkout -b MPCSOK 4d17c6476135723a2ed3d596dbba1709d0c6b4ae
+#git checkout -b MPCSOK 4d17c6476135723a2ed3d596dbba1709d0c6b4ae
 cd thirdparty/linux
 bash eigen.get
 bash function2.get
