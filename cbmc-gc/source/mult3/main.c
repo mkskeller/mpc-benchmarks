7f47bf920ebd131a02919982bc8c32626dfb7d04
Marcel Keller <mks.keller@gmail.com>
2020-08-25 03:29:14 +1000

    Remove unused frameworks.

--- cbmc-gc/source/mult3/main.c
+++
@@ -1,9 +0,0 @@
-
-// multiplies together three numbers
-// limited to ANSI_C types
-
-int mpc_main(int INPUT_A, int INPUT_B, int INPUT_C) {
-
-    return INPUT_A * INPUT_B * INPUT_C;
-}
-
--- jiff/vim.sh
+++
@@ -1,5 +0,0 @@
-# add simple syntax highlighting and vim bindings
-cd
-git clone https://github.com/marsella/vim.git
-mv vim/.vim* .
-
